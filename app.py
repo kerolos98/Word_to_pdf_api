1e451028a69ad4770271663ac6b4a63654f2e559
kerolos emad <33869460+kerolos98@users.noreply.github.com>
2022-12-12 17:52:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,11 @@
 import sys
 import subprocess
 import re
-from flask import Flask ,render_template,request,redirect
+from flask import Flask ,render_template,request,redirect,send_file,Flask,send_from_directory
 app=Flask(__name__,static_url_path='/static')
 @app.route("/", methods=["GET","POST"])
 def index():
-    transcript=""
-    graphSON=""
-    if request.method=="POST":
-        print("FORM DATA RECEIVE")
-        if "file" not in request.files:
-            return redirect(request.url)
-        file=request.files["file"]
-        if file.filename=="":
-            return redirect(request.url)
-        if file:
-    
-            recognizer=sr.Recognizer()
-            audio=sr.AudioFile(file)
-            with audio as source:
-                data =recognizer.record(source)
-            text=recognizer.recognize_google(data ,key=None)
-            transcript=text
-            list_text=text.split(' ')
-            nonPunct = re.compile('.*[A-Za-z0-9].*')  
-            filtered = [w for w in list_text if nonPunct.match(w)]
-            counts = Counter(filtered)
-            topcounts=topwords(counts)
-            words_pd=pd.DataFrame()
-            words_pd["words"]=topcounts.keys()
-            words_pd["counts"]=topcounts.values()
-            fig = px.bar(words_pd, x='words', y='counts', barmode='group')
-            graphSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
            
 
 
